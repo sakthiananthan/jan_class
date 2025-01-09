3db86b398d0d208a2586b0c69891d5c0e8adef48
sakthi <sakthisakthiananthan@gmail.com>
2025-01-09 09:37:00 +0530

    adding test.py

---
+++ test.py
@@ -0,0 +1,47 @@
+# import json
+
+# with open("data/fees.json") as f:
+#     fees_data=json.load(f)
+    
+
+# stud_dict={
+#     "name":input("enter your name : "),
+#     "course":input("enter course : ")
+# }
+
+# if stud_dict["course"]=="hindi":
+#     stud_dict["fees"]=fees_data["hindi"]
+    
+# print(stud_dict)
+
+
+# coordinates=(4,5)
+
+# coordinates=list(coordinates)
+# coordinates.append(6)
+# print(tuple(coordinates))
+
+# coordinates = [(4,5), (6,7), (9,10),"sakthi"]
+# # print(coordinates[1][0])
+
+# print(coordinates[-1].upper())
+
+# coordinates = [(4,5),[1,23,4,5,"sakthi"],[1,[2,3,4],[4,5,(6,7,8)]]]
+# print(coordinates[2][2][2][1])
+
+
+# def sayhi():
+#     print("hi")
+    
+# print("top")
+# sayhi()
+# print("below")
+
+# def sayhi(user,age="32"):
+#     print("hi " + user)
+#     print("you are "+age+" years old")
+ 
+# print("top")
+# sayhi("35","anand")
+# print("below")
+
